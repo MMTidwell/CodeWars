4d019e55f6b4cf8fd445ba64701a3efccb805074
Mittsy Tidwell <mtidwell506@gmail.com>
2017-05-03 11:39:46 -0500

    first commit

---
+++ javascript/codewars.js
@@ -0,0 +1,16 @@
+// convert string to lower case
+// 8 kyu
+function convertLowerCase(str) {
+	console.log(str.toLowerCase());
+}
+convertLowerCase("Hello world!")
+
+
+// Convert a string to all uppercase letters.
+// 8 kyu
+function capitalizeEachWord(str) {
+	console.log(str.toUpperCase());
+}
+capitalizeEachWord("Hello world!")
+
+
